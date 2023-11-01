68c54dd6ca503c0967d1a1fc0eb5cbbcf9b7e85f
kirubel-web <kirubelalemu119@gmail.com>
2023-11-01 23:56:09 +0300

    setting fix

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permission.AllowAny",
+        "rest_framework.permissions.AllowAny",
     ]
 }
 
